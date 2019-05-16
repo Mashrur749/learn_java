28f9ccd6713d5134b8fdf1da970315063c5eb825
Musaddiqur Rahman <mashrur749@gmail.com>
2019-05-16 07:25:01 -0400

    basic lamda

--- functionalProgramming/src/Main.java
+++ functionalProgramming/src/Main.java
@@ -1,11 +1,11 @@
-import java.util.function.Function;
 
 public class Main {
 
     public static void main(String[] args) {
-        Summable obj = (x, y) ->{
-            return ((x + y) % 2) == 0;
-        };
+        Summable obj = (x, y) ->
+            ((x + y) % 2) == 0;
+
         System.out.println(obj.evenSum(2,5));
+
     }
 }
