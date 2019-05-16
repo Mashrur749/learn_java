f4a1fa00627f697bd2023221f39b4fb15dda8a15
Musaddiqur Rahman <mashrur749@gmail.com>
2019-05-16 09:52:53 -0400

    Runable lamda

--- functionalProgramming/src/Main.java
+++ functionalProgramming/src/Main.java
@@ -2,10 +2,19 @@
 public class Main {
 
     public static void main(String[] args) {
-        Summable obj = (x, y) ->
-            ((x + y) % 2) == 0;
-
+        Summable obj = (x, y) ->{
+            return ((x + y) % 2) == 0;
+        };
+        //lamda can be used to threads and all the other
+        //single method interfaces.
+        Runnable x = () -> print("Hello there");
+        x.run();
         System.out.println(obj.evenSum(2,5));
 
+
+
+    }
+    private static void print(String text){
+        System.out.println(text);
     }
 }
